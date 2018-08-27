649338e1525a966f037998e82c5e75a1d48049a5
sebafonte <sebafonte@gmail.com>
2018-08-27 16:09:02 -0300

    Login page update.

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const session = require('express-session');
 const querystring = require('querystring');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const app = express()
 
 function initializeDatabase() {	
@@ -17,28 +18,45 @@ function initializeDatabase() {
 	});
 
 	User = db.model('users', userSchema);	
+
+	//var value = new User({ login: "seba", password: "pepe", locked: false });
+	//value.save(printBDError);
+}
+
+function printBDError (err, result) {
+      if (err) throw err;
+      console.log(result);
 }
 
 // Init database
 var userSchema, User;
 const db = mongoose.createConnection('mongodb://localhost/rhdb');
 // Init system
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(cookieParser());
 app.set('view engine', 'ejs')
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
     saveUninitialized: true
 }));
+
+
 initializeDatabase();
+
 //http.globalAgent.maxSockets = 50;
 
 
 // Authentication and Authorization Middleware
 var auth = function(req, res, next) {
-  if (req.session && req.session.user === "amy" && req.session.admin)
+  //if (req.session && req.session.user === "amy" && req.session.admin)
+  if (req.session && req.session.user && req.session.admin)  
     return next();
   else
-    return res.sendStatus(401);
+    //return res.sendStatus(401);
+   return res.redirect("/index")
 };
 
 // Index pages
@@ -50,40 +68,36 @@ app.get('/index', function (req, res) {
   res.render('index');
 })
 
-app.get('/home', auth, function (req, res) {
+app.get('/users', auth, function (req, res) {
+  res.render('users');
+})
+
+app.get('/home', function (req, res) {
   if (req.session.admin)
-  	res.render('home');
+  	res.render('/home');
   else
-  	res.render('login');
+  	res.redirect("/")
 })
 
 // Login endpoint
 app.post('/login', function (req, res) {
-
-	function cb (err, body) {
-	  // Log body
-  	  console.log('err: ' + err);
-  	  console.log('body: ' + body);
-
-	  // Filter empty stuff
-	  if (!req.body.username || !req.body.password)
+	// Check null value
+ 	if (!req.body.username || !req.body.password)
 	    res.send('login failed');
-	  // Select user
-	  User.find( 
-	    { login: args.login } , 
-	    function(err, entities) {
-	      var result = entities[0];
-	      if (result != null) {
-		req.session.username = "amy";
-		req.session.admin = true;
-		res.send("login success!");
-	      }
-	      else
-		res.send('login failed');
-	    }); 
-	}
-
-  	//jsonBody(req, res, cb);
+	else
+	// Select user
+	User.find( 
+		{ login: req.body.username } , 
+		function(err, entities) {
+			var result = entities[0];
+			if (result != null && (req.body.password == result.password)) {
+				req.session.username = req.body.username;
+				req.session.admin = true;
+				res.send("login success!");
+			}
+			else
+			res.send('login failed');
+		}); 
 });
  
 // Logout endpoint
