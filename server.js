5897d25d0e988f92def3f46c4bc0a1d10d06b2ea
sebafonte <sebafonte@gmail.com>
2018-08-29 15:14:15 -0300

    Changes to directory structure.

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// localhost:3000/login?username=amy&password=amypassword
-// localhost:3000/home
 
 const express = require('express')
 const session = require('express-session');
@@ -8,7 +6,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express()
+const app = express();
+const sitePort = 80;
+
 
 function initializeDatabase() {	
     userSchema = mongoose.Schema({
@@ -35,11 +35,7 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(cookieParser());
 app.set('view engine', 'ejs')
-app.use(session({
-    secret: '2C44-4D44-WppQ38S',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(session({ secret: '2C44-4D44-WppQ38S', resave: true, saveUninitialized: true }));
 
 
 initializeDatabase();
@@ -53,6 +49,10 @@ const auth = function(req, res, next) {
     return res.redirect("/index")
 };
 
+// Static content
+app.use(express.static('/public/images'));
+app.use(express.static('/public/css'));
+app.use(express.static('public'));
 	
 // Index pages
 app.get('/', function (req, res) {
@@ -69,42 +69,56 @@ app.get('/index', function (req, res) {
     res.render('index', { errorMessage: message });
 })
 
-app.get('/users', auth, function (req, res) {
-  res.render('users');
-})
-
-
-// Login endpoint
+// Login
 app.post('/login', function (req, res) {
-	// Check null value
- 	if (!req.body.username || !req.body.password)
-	    res.send('login failed');
-	else
-	// Select user
-	User.find( 
-		{ login: req.body.username } , 
-		function(err, entities) {
-			var result = entities[0];
-			if (result != null && (req.body.password == result.password)) {
-				req.session.username = req.body.username;
-				req.session.admin = true;
-				req.session.errorMessage = "Welcome, " + req.session.username;
-				res.redirect("/index");
-			}
-			else {
-				req.session.errorMessage = "Login failed";		
-				res.redirect("/index");				
-			}
-
-		}); 
-});
- 
-// Logout endpoint
-app.post('/logout', function (req, res) {
-  req.session.destroy();
-  res.redirect("/index");
+  // Check null value
+  if (!req.body.username || !req.body.password)
+    res.send('login failed');
+  else
+    // Select user
+    User.find( 
+      { login: req.body.username }, 
+      function(err, entities) {
+        var result = entities[0];
+        if (result != null && (req.body.password == result.password)) {
+          req.session.username = req.body.username;
+          req.session.admin = true;
+          req.session.errorMessage = "Welcome, " + req.session.username;
+        }
+        else {
+          req.session.admin = false;
+          req.session.errorMessage = "Login failed";		
+        }
+        
+	res.redirect("/index");
+      }); 
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// Logout
+app.route('/logout')
+  .get(function(req, res) {
+    req.session.destroy();
+    res.redirect("/index");
+  })
+  .post(function (req, res) {
+    req.session.destroy();
+    res.redirect("/index");
+  });
+
+// Main functionality
+app.get('/entities', auth, function (req, res) {
+  res.render('entities', { entities: [ 1, 2, 3 ] });
+  return;
+});
+
+app.get('/users', auth, function (req, res) {
+  res.render('users', { users: [ 1, 2, 3 ] });
+});
+
+
+// Application start
+app.listen(sitePort, function () {
+  console.log('Example app listening on port ' + sitePort.toString() + '!')
+});
+
+
