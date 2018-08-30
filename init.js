9fb34dc7f1cd132ef5db61c6a050754b830cdb29
sebafonte <sebafonte@gmail.com>
2018-08-30 02:20:12 -0300

    Initialization for entities.

--- init.js
+++ init.js
@@ -3,20 +3,20 @@ const db = mongoose.createConnection('mongodb://localhost/rhdb');
 
 
 function printBDError (err, result) {
-      if (err) throw err;
-      console.log(result);
+  if (err) throw err;
+  console.log(result);
 }
 
 function initializeRoot() {	
-	const userSchema = mongoose.Schema({
-		login: { type: String, trim: true, index: true },
-		password: String,
-		locked: Boolean
-	});
+  const userSchema = mongoose.Schema({
+    login: { type: String, trim: true, index: true },
+    password: String,
+    locked: Boolean
+  });
 
-	const User = db.model('users', userSchema);	
-	const value = new User({ login: "admin", password: "pepe", locked: false });
-	value.save(printBDError);
+  const User = db.model('users', userSchema);	
+  const value = new User({ login: "admin", password: "pepe", locked: false });
+  value.save(printBDError);
 }
 
 
